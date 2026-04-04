b9b3430edbde3f7966f5d34159ce1ccf65a79016
Nooruddin Md Afsar <mdafsar786noor@gmail.com>
2026-04-04 16:56:47 +0530

    code added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
   return (
     <>
+      <AppRoutes />
       <h1 className='bg-red-500 text-white p-4 text-center font-bold'>My Dashboard</h1>
     </>
   )
